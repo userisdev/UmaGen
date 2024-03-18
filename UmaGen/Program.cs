22a05e13a3fc43f3822e1475718bce72cce18348
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-18 14:23:54 +0900

    first commit.

---
+++ UmaGen/Program.cs
@@ -0,0 +1,16 @@
+﻿var baseUrl = "https://tabbotdev-umamusume.netlify.app/";
+
+var dir = new DirectoryInfo(args.FirstOrDefault());
+
+var files = dir.GetFiles("*",SearchOption.TopDirectoryOnly);
+
+var list = new List<string>();  
+
+foreach(var (file,index) in files.Select((e, i) => (e, i)))
+{
+    list.Add($"umamusume,{Path.GetFileNameWithoutExtension(file.Name)},{baseUrl}{index:000}.jpg");
+    var movedPath = Path.Combine(file.DirectoryName, $"{index:000}.jpg");
+    file.MoveTo(movedPath);
+}
+
+File.WriteAllLines("list.csv", list);
